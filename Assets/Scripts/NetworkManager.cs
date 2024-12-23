f8f1e127c2efb99537a95ede0ceee780d5352800
BackdevHong <ghddls0704@kakao.com>
2024-12-24 00:46:15 +0900

    change unity 6 and netcode

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -26,31 +26,16 @@ public class NetworkManager : Mirror.NetworkManager
 
     public override void OnClientConnect()
     {
-        base.OnClientConnect();
         Debug.Log("클라이언트 서버 연결");
     }
 
     public override void OnClientDisconnect()
     {
-        base.OnClientDisconnect();
         Debug.Log("클라이언트 서버 연결 해제");
     }
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        base.OnServerAddPlayer(conn);
         Debug.Log("플레이어가 서버에 연결하였습니다.");
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
